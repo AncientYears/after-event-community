b65efa6a6d786069c759d2591c3a41d7bceec391
Lubomir <lubomir.yudenko@gmail.com>
2019-07-14 14:53:54 +0300

    Good commit

--- index.js
+++ index.js
@@ -1,29 +1,40 @@
 const Discord = require('discord.js');
-const fs = require('fs');
 const client = new Discord.Client();
 
 //Set up config
-let token = JSON.parse(fs.readFileSync('bot.json')).token;
-if (token == null || token == undefined) {
-	console.err("Token is null.");
-	process.exit(-1);
+const config = require("./config.js")
+// config.js template
+/*
+  module.exports = {
+    "token":"TOKEN_HERE",
+    "prefix":"!"
+  }
+*/
+if (!config.token) {
+  console.err("Invalid token :0");
+  process.exit(-1);
 }
-
+client.login(config.token);
+Commands = {
+  "test": async (msg) => {
+    msg.channel.send("Oh look! G's stupid test message!")
+  }
+}
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
   client.user.setStatus('online');
   client.user.setPresence({
-		game: {
-            name: 'PARTY POOPER 3000',
-            type: 0
-        }
+    game: {
+      name: 'PARTY POOPER 3000',
+      type: 0
+    }
   });
 });
-
 client.on("message", msg => {
-  if (msg.content === "!test aec") {
-    msg.reply("The After Event Community Bot is up and running!");
+  if (!msg.guild || msg.author.bot) {
+    return;
+  }
+  if (Object.keys(Commands).includes(msg.content.substr(config.prefix.length).split(" ")[0]) && msg.content.substr(0, config.prefix.length) == config.prefix) {
+    Commands[msg.content.split(" ")[0]](msg)
   }
 });
-
-client.login(token);
